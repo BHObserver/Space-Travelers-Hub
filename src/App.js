e7f03942ac4774e1c196d4f9c988ee839ad4aacd
BHObserver <BHObserver0@gmail.com>
2023-10-18 00:23:54 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import {
-  createBrowserRouter, createRoutesFromElements, Route, RouterProvider,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Dragons from './components/Dragons/Dragons';
-import Missions from './components/Missions/Missions';
 import Profile from './components/Profile/Profile';
 import RocketList from './components/rocket/RocketList';
+import MissionsContainer from './components/Missions/MissionsContainer'; // Import MissionsContainer
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<Navbar />}>
-      <Route path="/" element={<RocketList />} />
-      <Route path="/missions" element={<Missions />} />
-      <Route path="/Dragons" element={<Dragons />} />
-      <Route path="/my-profile" element={<Profile />} />
-    </Route>,
-  ),
-);
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<RocketList />} />
+        <Route path="/missions" element={<MissionsContainer />} />
+        <Route path="/dragons" element={<Dragons />} />
+        <Route path="/my-profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 export default App;
