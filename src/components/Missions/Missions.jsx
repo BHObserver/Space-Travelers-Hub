90697f4107b50ddc13098d0950b9df88535ab277
BHObserver <BHObserver0@gmail.com>
2023-10-17 18:32:56 +0600

    Update Missions.jsx

--- src/components/Missions/Missions.jsx
+++ src/components/Missions/Missions.jsx
@@ -11,7 +11,15 @@ function MissionRow({ mission, onJoin, onCancel }) {
         <p className="description">{mission.description}</p>
       </td>
       <td>
-        
+        <div className="status">
+          {mission.reserved ? (
+            <p className="active-member">Active member</p>
+          ) : (
+            <p className="not-member">NOT a member</p>
+          )}
+        </div>
+      </td>
+      
     </tr>
   );
 }
