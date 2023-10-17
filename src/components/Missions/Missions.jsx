4767bbac1e7bd12ab8ae757baf406fda15764b6a
BHObserver <BHObserver0@gmail.com>
2023-10-17 18:56:33 +0600

    Update Missions.jsx

--- src/components/Missions/Missions.jsx
+++ src/components/Missions/Missions.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import './Missions.css';
+import './missions.css';
+import { cancelMission, joinMission } from '../../redux/missions/missionsSlice';
 
 function MissionRow({ mission, onJoin, onCancel }) {
   return (
     <tr key={mission.mission_id}>
-      <td className="mission-name">
-        <h3>{mission.mission_name}</h3>
+      <td className="mission-name-cell">
+        <h3 className="mission-name">{mission.mission_name}</h3>
       </td>
-      <td>
-        <p className="description">{mission.description}</p>
+      <td className="mission-description-cell">
+        <p className="mission-description">{mission.description}</p>
       </td>
-      <td>
-        <div className="status">
+      <td className="mission-status-cell">
+        <div className="mission-status">
           {mission.reserved ? (
-            <p className="active-member">Active member</p>
+            <p className="active-member-status">Active member</p>
           ) : (
-            <p className="not-member">NOT a member</p>
+            <p className="not-member-status">NOT a member</p>
           )}
         </div>
       </td>
-      <td className="mission-btn">
+      <td className="mission-action-cell">
         {!mission.reserved ? (
-          <button type="button" className="join-mission" onClick={() => onJoin(mission.mission_id)}>
+          <button type="button" className="join-mission-button" onClick={() => onJoin(mission.mission_id)}>
             Join Mission
           </button>
         ) : (
-          <button type="button" className="leave-mission" onClick={() => onCancel(mission.mission_id)}>
+          <button type="button" className="leave-mission-button" onClick={() => onCancel(mission.mission_id)}>
             Leave Mission
           </button>
         )}
@@ -34,14 +35,14 @@ function MissionRow({ mission, onJoin, onCancel }) {
   );
 }
 
-function Missions({ missions, onJoin, onCancel }) {
+function Missions({ missions, dispatch }) {
   return (
     <div className="mission-page">
-      <table className="missionTable">
-        <thead className="missionThead">
+      <table className="mission-table">
+        <thead className="mission-header">
           <tr>
             <th width="10%">
-              <h3>Missions</h3>
+              <h3>Mission</h3>
             </th>
             <th width="65%">
               <h3>Description</h3>
@@ -49,21 +50,20 @@ function Missions({ missions, onJoin, onCancel }) {
             <th width="12.5%">
               <h3>Status</h3>
             </th>
-            <th width="12.5%"></th>
           </tr>
         </thead>
         <tbody>
           {missions.length === 0 ? (
             <tr>
-              <td colSpan="4">Loading missions...</td>
+              <td colSpan="3">Loading missions...</td>
             </tr>
           ) : (
             missions.map((mission) => (
               <MissionRow
                 key={mission.mission_id}
                 mission={mission}
-                onJoin={onJoin}
-                onCancel={onCancel}
+                onJoin={() => dispatch(joinMission(mission.mission_id))}
+                onCancel={() => dispatch(cancelMission(mission.mission_id))}
               />
             ))
           )}
