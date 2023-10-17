8ea169200ce1e2c4c075d5d32fd790cb2ee1021f
BHObserver <BHObserver0@gmail.com>
2023-10-17 18:39:01 +0600

    Update missionsSlice.js

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -7,3 +7,33 @@ const initialState = {
   missionData: [],
   selectedMissions: [],
 };
+
+export const fetchMissionsAsync = createAsyncThunk('missions/fetchMissions', async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    throw error; // You can handle errors in the component that dispatches the action.
+  }
+});
+
+const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+  reducers: {
+    joinMission: (state, action) => {
+      const missionId = action.payload;
+      if (!state.selectedMissions.includes(missionId)) {
+        state.selectedMissions.push(missionId);
+      }
+    },
+    cancelMission: (state, action) => {
+      const missionId = action.payload;
+      state.selectedMissions = state.selectedMissions.filter((id) => id !== missionId);
+    },
+  },
+  
+});
+
+export default missionsSlice.reducer;
+export const { cancelMission, joinMission } = missionsSlice.actions;
