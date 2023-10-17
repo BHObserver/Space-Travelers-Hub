7674c334b64c6500cd5e20a83b9b2ec62e528a57
BHObserver <BHObserver0@gmail.com>
2023-10-17 17:11:21 +0600

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import logo from './planet.png'; // Assuming the image file is in the correct location.
 import './navbar.css';
 
 const Navbar = () => (
-  <nav className="navbar-container">
-    <div className="logo-container">
-      <div className="brand-section">
-        <img src={logo} alt="logo" className="logo-image" />
-        <div className="logo-title">Space Travelers&apos; Hub</div>
+  <>
+    <nav className="navbar-container">
+      <div className="logo-container">
+        <div className="brand-section">
+          <img src={logo} alt="logo" className="logo-image" />
+          <div className="logo-title">Space Travelers&apos; Hub</div>
+        </div>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <NavLink className="nav-link" activeclassname="active-link" to="/">Rockets</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" activeclassname="active-link" to="/missions">Missions</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" activeclassname="active-link" to="/Dragons">Dragons</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" activeclassname="active-link" to="/my-profile">My Profile</NavLink>
+          </li>
+        </ul>
       </div>
-      <ul className="nav-menu">
-        <li className="nav-item">
-          <NavLink className="nav-link" activeClassName="active-link" to="/">Rockets</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" activeClassName="active-link" to="/missions">Missions</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" activeClassName="active-link" to="/dragons">Dragons</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" activeClassName="active-link" to="/my-profile">My Profile</NavLink>
-        </li>
-      </ul>
+    </nav>
+    <div>
+      <Outlet />
     </div>
-  </nav>
+  </>
 );
-
 export default Navbar;
