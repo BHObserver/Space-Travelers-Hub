672dc4c1ab76b3e5a250d19c96f2e9e60e402f32
BHObserver <BHObserver0@gmail.com>
2023-10-18 00:23:56 +0600

    Update MissionsContainer.js

--- src/components/Missions/MissionsContainer.js
+++ src/components/Missions/MissionsContainer.js
@@ -1,18 +1,20 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissionsAsync } from '../../redux/missions/missionsSlice'; // Remove unused actions
+import { fetchMissionsAsync } from '../../redux/missions/missionsSlice';
 import Missions from './Missions';
 
 function MissionsContainer() {
   const missions = useSelector((state) => state.missions.missionData);
+  console.log('Missions data:', missions); // Add this line
+
   const selectedMissions = useSelector((state) => state.missions.selectedMissions);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!missions.length) {
+    if (!missions || missions.length === 0) {
       dispatch(fetchMissionsAsync());
     }
-  }, [dispatch, missions.length]);
+  }, [dispatch, missions]);
 
   return <Missions missions={missions} selectedMissions={selectedMissions} />;
 }
