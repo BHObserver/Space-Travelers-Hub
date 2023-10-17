fde91a98149dfdfdd11203d53ae1dbdc9a097c3b
BHObserver <BHObserver0@gmail.com>
2023-10-17 18:32:40 +0600

    Update Missions.jsx

--- src/components/Missions/Missions.jsx
+++ src/components/Missions/Missions.jsx
@@ -3,6 +3,11 @@ import './Missions.css';
 
 function MissionRow({ mission, onJoin, onCancel }) {
   return (
-    
+    <tr key={mission.mission_id}>
+      <td className="mission-name">
+        <h3>{mission.mission_name}</h3>
+      </td>
+      
+    </tr>
   );
 }
