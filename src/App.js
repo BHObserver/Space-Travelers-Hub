722c216747dd093bbbf213dc871f4beeb1428799
BHObserver <BHObserver0@gmail.com>
2023-10-17 18:02:43 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import {
 } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Dragons from './components/Dragons/Dragons';
+import Missions from './components/Missions/Missions';
+import Profile from './components/Profile/Profile';
+import RocketList from './components/rocket/RocketList';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Navbar />}>
-      <Route path="/" element={<Dragons />} />
+      <Route path="/" element={<RocketList />} />
       <Route path="/missions" element={<Missions />} />
       <Route path="/Dragons" element={<Dragons />} />
       <Route path="/my-profile" element={<Profile />} />
