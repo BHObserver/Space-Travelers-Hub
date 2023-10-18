b44651fb15e2a97783bee84b5c59389fe17d34ef
BHObserver <BHObserver0@gmail.com>
2023-10-18 23:21:42 +0600

    Create Profiles.jsx

---
+++ src/components/Profile/Profiles.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+/* import MyRockets from './MyRockets'; */
+import MyMissions from './MyMissions';
+import './profiles.css';
+
+function MyProfiles() {
+  return (
+    <div className="profile-container">
+      {/* <MyRockets /> */}
+      <MyMissions />
+    </div>
+  );
+}
+
+export default MyProfiles;
