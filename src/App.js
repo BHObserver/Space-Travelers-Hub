a303da8c98d007a4350c9980bcb3abc7d1c101a0
samiullahtufail <sami.u.tufail@samiullahtufails-MacBook-Pro.local>
2023-10-20 01:15:11 +0500

    making changes in the app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   createBrowserRouter, createRoutesFromElements, Route, RouterProvider,
 } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import Navbar from './components/Navbar/Navbar';
 import Dragons from './components/Dragons/Dragons';
-import MyProfileDragon from './components/myprofile/myProfileDragon';
+import MyProfiles from './components/Profile/Profiles';
+import RocketList from './components/rocket/RocketList';
+import MissionsContainer from './components/Missions/MissionsContainer';
 import { DragonFetch } from './components/redux/Dragon/dragonSlics';
 import './App.css';
 
+
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<Navbar />}>
-      <Route path="/" element={<Dragons />} />
-      <Route path="/missions" element={<Dragons />} />
-      <Route path="/Dragons" element={<Dragons />} />
-      <Route path="/my-profile" element={<MyProfileDragon />} />
+    <Route path="/" element={<Navbar />}>
+      <Route path="/" element={<RocketList />} />
+      <Route path="/missions" element={<MissionsContainer />} />
+      <Route path="/dragons" element={<Dragons />} />
+      <Route path="/my-profile" element={<MyProfiles />} />
     </Route>,
   ),
 );
 
 function App() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
   useEffect(() => {
     dispatch(DragonFetch());
   }, []);
